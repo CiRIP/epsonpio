2c147af9ddd4f546902ebad4bcfd9c0dd9d48f60
Ciprian Ionescu <me@ciprian-ionescu.com>
2026-08-10 23:05:50 +0200

    add license headers to files

--- fls/entrypoint.s
+++ fls/entrypoint.s
@@ -1,3 +1,18 @@
+; SPDX-License-Identifier: AGPL-3.0-or-later
+; Copyright (C) 2026 Ciprian Ionescu <me@ciprian-ionescu.com>
+;
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU Affero General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+; GNU Affero General Public License for more details.
+;
+; You should have received a copy of the GNU Affero General Public License
+; along with this program. If not, see <https://www.gnu.org/licenses/>.
 ; drv_asm.s  1998.04.27
 ;            2002.05.23 modified register assignment 
 ; asm entry program for ICD33/MON33 flash command
--- scripts/clockspeed.s
+++ scripts/clockspeed.s
@@ -1,3 +1,18 @@
+; SPDX-License-Identifier: AGPL-3.0-or-later
+; Copyright (C) 2026 Ciprian Ionescu <me@ciprian-ionescu.com>
+;
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU Affero General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+; GNU Affero General Public License for more details.
+;
+; You should have received a copy of the GNU Affero General Public License
+; along with this program. If not, see <https://www.gnu.org/licenses/>.
 	ld.w	%r5,%sp		; save SP
 	xld.w	%r4,0x0800
 	ld.w	%sp,%r4		; set SP
